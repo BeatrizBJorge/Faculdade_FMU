7ba64822f32beabaa2a96564a39ed8e49e606b2d
BeatrizBJorge <128194589+BeatrizBJorge@users.noreply.github.com>
2024-03-07 13:59:52 -0300

    Create AtribuicaoUsuario.c

---
+++ Algoritmos_e_Programacao/Aula02_03-03-22/AtribuicaoUsuario.c
@@ -0,0 +1,14 @@
+//Programa para ler o número que o usuário digitar
+
+#include<stdio.h>
+
+int n;
+
+int main(){
+  printf("Digite um número\n");
+  scanf("%d",&n);
+  
+  printf("\nO numero é: %d\n",n);
+  
+  return 0;
+}
