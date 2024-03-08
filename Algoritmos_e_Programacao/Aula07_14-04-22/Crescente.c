92b725ab1e525e5571974e3b94c23f2a82498e09
BeatrizBJorge <128194589+BeatrizBJorge@users.noreply.github.com>
2024-03-08 17:34:37 -0300

    Create Crescente.c

---
+++ Algoritmos_e_Programacao/Aula07_14-04-22/Crescente.c
@@ -0,0 +1,15 @@
+/*
+Faça um programa que imprima os números de
+1 até 100.
+*/
+#include<stdio.h>
+
+int i;
+
+int main (){
+  for (i=1;i<=100;i++){
+    printf("\n %d \n", i);
+  }
+
+  return 0;
+}
