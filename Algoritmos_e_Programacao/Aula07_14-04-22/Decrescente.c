f862c9b682156cb55d1a3dd9b71639ddad6cee1d
BeatrizBJorge <128194589+BeatrizBJorge@users.noreply.github.com>
2024-03-08 17:33:18 -0300

    Create Decrescente.c

---
+++ Algoritmos_e_Programacao/Aula07_14-04-22/Decrescente.c
@@ -0,0 +1,15 @@
+/*
+Faça um programa que imprima os números de
+100 até 1.
+*/
+#include<stdio.h>
+
+int i;
+
+int main(){
+  for(i=100;i>=1;i--){
+    printf("\n %d \n", i);
+  }
+
+  return 0;
+}
